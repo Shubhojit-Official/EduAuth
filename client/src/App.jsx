14b170f5a51561271cae41d58902b0d45eb7642a
SANJOY <paulsanjoy2923@gmail.com>
2025-09-13 20:14:34 +0530

    file strusture created

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='bg-yellow-400'>
+      hiii
+    </div>
+    </>
+  )
+}
+
+export default App
